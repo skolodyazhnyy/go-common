cb881a15492ea34caaa3aaaa22b7d4577674e050
Jorge Cuerdo <jcuerdo@magento.com>
2018-02-22 15:37:23 +0100

    Add auth

---
+++ auth/auth.go
@@ -0,0 +1,36 @@
+package auth
+import (
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
+	"fmt"
+)
+
+type Auth struct{
+	Environment string
+	Url string
+	Client string
+	Secret string
+}
+
+func NewAuth(environment string,url string,client string,secret string) *Auth{
+	return &Auth{
+		environment,
+		url,
+		client,
+		secret,
+		}
+}
+
+func (auth *Auth) GetAuthorizedClient() (clientcredentials.Config, error) {
+	if auth.Client == "" || auth.Secret == "" {
+		return clientcredentials.Config{}, fmt.Errorf("Configuration parameters for Auth must be set")
+	}
+	oauth2.RegisterBrokenAuthHeaderProvider(auth.Url)
+	configuration := clientcredentials.Config{
+		ClientID:     auth.Client,
+		ClientSecret: auth.Secret,
+		TokenURL:     auth.Url,
+	}
+
+	return configuration, nil
+}
