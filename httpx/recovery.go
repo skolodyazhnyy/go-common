6e84a77143ecfc59a159ca5901e2e6143e771110
Sergey Kolodyazhnyy <skolodyazhny@magento.com>
2018-07-25 23:55:03 +0200

    Add panic location to recovery middleware

--- httpx/recovery.go
+++ httpx/recovery.go
@@ -3,6 +3,8 @@ package httpx
 import (
 	"fmt"
 	"net/http"
+	"path"
+	"runtime"
 )
 
 type recovery interface {
@@ -15,12 +17,34 @@ func Recovery(log recovery) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			defer func() {
 				if err := recover(); err != nil {
-					log.Error(fmt.Sprint(err), nil)
+					log.Error(fmt.Sprint(err), map[string]interface{}{"panic": location(5)})
 					rw.WriteHeader(http.StatusInternalServerError)
 				}
 			}()
 
 			h.ServeHTTP(rw, req)
 		})
+	}
+}
+
+func location(skip int) map[string]interface{} {
+	fpcs := make([]uintptr, 1)
+
+	n := runtime.Callers(skip, fpcs)
+	if n == 0 {
+		return nil
+	}
+
+	fun := runtime.FuncForPC(fpcs[0] - 1)
+	if fun == nil {
+		return nil
+	}
+
+	file, line := fun.FileLine(fpcs[0] - 1)
+
+	return map[string]interface{}{
+		"function": path.Base(fun.Name()),
+		"file":     path.Base(file),
+		"line":     line,
 	}
 }
