aaddbd03454fcbc95bad2caec6d62dc4d77daf45
Jaime Lopez <aguilar@adobe.com>
2019-08-12 16:34:38 +0200

    Adds IAM Role crendential handler for AWSx package

--- awsx/credentials.go
+++ awsx/credentials.go
@@ -2,13 +2,16 @@ package awsx
 
 import (
 	"errors"
+
 	"github.com/aws/aws-sdk-go/aws/credentials"
 )
 
+// Defined errors
 var (
 	ErrInvalidCredentialsType = errors.New("credentials type is invalid (should be: shared, static or environment)")
 )
 
+// List of crendential types
 const (
 	// Shard credentials load ~/.aws/credentials file
 	CredentialsTypeShared string = "shared"
@@ -18,6 +21,9 @@ const (
 
 	// Environment credentials use environment variables
 	CredentialsTypeEnvironment string = "environment"
+
+	// Gets credentials from IAM Role assigned to EC2 instance
+	CredentialsTypeRole string = "role"
 )
 
 // Credentials from AWS Config
@@ -27,8 +33,10 @@ func Credentials(c Config) (*credentials.Credentials, error) {
 		return credentials.NewStaticCredentials(c.AccessKey, c.SecretKey, c.Token), nil
 	case CredentialsTypeEnvironment:
 		return credentials.NewEnvCredentials(), nil
-	case CredentialsTypeShared, "":
+	case CredentialsTypeShared:
 		return credentials.NewSharedCredentials(c.CredentialsFilename, c.Profile), nil
+	case CredentialsTypeRole, "":
+		return nil, nil
 	}
 
 	return nil, ErrInvalidCredentialsType
